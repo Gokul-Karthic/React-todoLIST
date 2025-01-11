49974728999b3406e2fd2a5006192272c5028dbf
Sukanta Majhi <71279597+sukantamajhi@users.noreply.github.com>
2025-01-11 22:41:08 +0000

    Issue fixed

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,8 +6,9 @@ const TodoList = (props) => {
       <div className="two-itemDiv">
         {props.item.map((listItem, i) => {
           return (
-            <li className="list-item">
-              {props.item}
+            <li key={i} className="list-item">
+              {/* we need to replace {props.item} with listItem */}
+              {listItem}
               <span>
                 <i
                   className="fa-solid fa-trash-can icon1"
